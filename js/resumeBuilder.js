94dd6f58c2918221c9dc6ad9fcac3e1465a21705
Chi Wong <wong.169@gmail.com>
2014-11-24 19:05:21 -0800

    Finish Lesson 1 Quiz 3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,18 @@
 //Lesson 1
 //Quiz 1
 
-var awesomeThoughts = "I am Chi Wong and I am AWESOME!";
-console.log(awesomeThoughts);
+//var awesomeThoughts = "I am Chi Wong and I am AWESOME!";
+//console.log(awesomeThoughts);
 
 //Quiz 2
 //$() is the jQuery selector
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
+//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+//$("#main").append(funThoughts);
+
+//Quiz 3
+var name = "Chi Wong"
+var role = "Web Developer"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
