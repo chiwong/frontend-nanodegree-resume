32075c1e8223d29d8c2bfe3ae79218b00868095b
Chi Wong <wong.169@gmail.com>
2014-11-28 14:03:22 -0800

    End of Lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,65 +1,65 @@
-//Lesson 1
-//Quiz 1
-
-//var awesomeThoughts = "I am Chi Wong and I am AWESOME!";
-//console.log(awesomeThoughts);
-
-//Quiz 2
-//$() is the jQuery selector
-//var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-//$("#main").append(funThoughts);
-
-//Quiz 3
-var name = "Chi Wong";
-var role = "Web Developer";
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-//Quiz 4
-var email = "wong.169@gmail.com";
-var picURL = "images/fry.jpg";
-var welcome = "Welcome to Chi's resume!";
-var skills = ["HTML", "CSS", "Python"];
-var contact = {
-	"mobile": "626-xxx-xxxx",
-	"email": "wong.169@gmail.com",
-	"github": "https://github.com/chiwong",
-	"twitter": "@_prodichi_",
-	"location": "Los Angeles"
-};
-var bio = {
-	"name": name,
-	"role": role,
-	"contact": contact,
-	"picURL": picURL,
-	"wecome": welcome,
-	"skills": skills
+var work = {
+	"jobs": [
+	{
+		"employer":"Los Angeles",
+		"title": "Engineer",
+		"location": "Los Angeles",
+		"dates": "2007-Current",
+		"description": "Engineering"
+	},
+	{
+		"employer":"Some past employer",
+		"title": "Asistant Engineer",
+		"location": "Columbus",
+		"dates": "2005-2007",
+		"description": "Engineering"
+	}
+	]
 }
-var formattedMobile = HTMLemail.replace("%data%", bio.contact.mobile);
-$("#topContacts").append(formattedMobile);
 
-var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
-$("#topContacts").append(formattedEmail);
+var projects = {
+	"projects": [
+	{
+		"title": "Hydrologic Calculator",
+		"date": 2013,
+		"description": "Hydrologic Calculator written in Python",
+		"images": "images.fry.jpg"
+	}
+	]
+}
 
-var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
-$("#topContacts").append(formattedTwitter);
+var bio = {
+	"name": "Chi Wong",
+	"role": "Engineer",
+	"welcomeMessage": "Hi, please browse my resume",
+	"contacts": {
+		"mobile": "626-xxx-xxxx",
+		"email": "blah@gmail.com",
+		"github": "https://github.com/chiwong",
+		"twitter": "@blah",
+		"location": "Los Angeles"
+		},
+	"skills": ["HTML", "CSS", "Python"],
+	"bioPic": "images.fry.jpg"
+}
 
-var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
-$("#topContacts").append(formattedGithub);
-
-var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
-$("#topContacts").append(formattedLocation);
-
-var formattedBioPic = HTMLbioPic.replace("%data%", picURL);
-$("#header").append(formattedBioPic);
-
-var formattedWelcome = HTMLWelcomeMsg.replace("%data%", welcome);
-$("#header").append(formattedWelcome);
-
-$("#header").append(HTMLskillsStart);
-
-var formattedSkills = HTMLskills.replace("%data%", skills);
-$("#skills").append(formattedSkills);
-
+var education = {
+	"schools": [
+	{
+		"name": "The Ohio State University",
+		"location": "Columbus, OH",
+		"degree": "Bachelors of Science",
+		"majors": ["Systems Engineering"],
+		"dates": 9999,
+		"url": "www.osu.edu"
+	}
+	],
+	"onlineCourse": [
+	{
+		"title": "Javascript Basics",
+		"school": "Udacity",
+		"dates": 2014, 
+		"url": "www.udacity.com/course/ud804"
+	}
+	]
+}
